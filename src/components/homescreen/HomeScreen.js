58812d865b88671f96d784da16674d48fbf41d2a
bhargav.i <bhargav.i@tadigital.com>
2021-06-02 10:52:02 +0530

    After adding rows

--- src/components/homescreen/HomeScreen.js
+++ src/components/homescreen/HomeScreen.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Nav from '../nav/nav';
 import Banner from '../banner/banner';
+import requests from '../../axios/requests'
+import Row from '../row/row';
 
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
       <Nav />
       <Banner />
+      <Row title="Netflix Originals" fetchUrl = {requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Top Rated" fetchUrl = {requests.fetchTopRated}/>
+      <Row title="Romance" fetchUrl = {requests.fetchRomanceMovies}/>
+      <Row title="Trending" fetchUrl = {requests.fetchTrending}/>
+      <Row title="Comedy" fetchUrl = {requests.fetchComedyMovies}/>
+      <Row title="Documentaries" fetchUrl = {requests.fetchDocumentaries}/>
+      <Row title="Horror" fetchUrl = {requests.fetchHorrorMovies}/>
+      <Row title="Action" fetchUrl = {requests.fetchActionMovies}/>
     </div>
   );
 };
