916fa05d3d752fa9c8ad774881f9fa0ddf673a7e
bhargav.i <bhargav.i@tadigital.com>
2021-06-09 16:30:34 +0530

    Changed profile screen UI

--- src/components/profilescreen/ProfileScreen.js
+++ src/components/profilescreen/ProfileScreen.js
@@ -20,6 +20,29 @@ const ProfileScreen = ()=> {
                     <div className="profileScreen__plans">
                         <h3>Plans</h3>
                         <p>Renewal Date</p>
+                        <div className="profileScreen__subscriptions">
+                            <div className="profileScreen__plan">
+                                <div className="profileScreen__plan-details">
+                                    <span className="profileScreen__plan-heading">Netflix Standard</span>
+                                    <span className="profileScreen__plan-subHeading">1080p</span>
+                                </div>                                
+                                <button className="profileScreen__plan-button">Subscribe</button>
+                            </div>
+                            <div className="profileScreen__plan">
+                                <div className="profileScreen__plan-details">
+                                    <span className="profileScreen__plan-heading">Netflix Basic</span>
+                                    <span className="profileScreen__plan-subHeading">1080p</span>
+                                </div>  
+                                <button className="profileScreen__plan-button">Subscribe</button>
+                            </div>
+                            <div className="profileScreen__plan profileScreen__plan--current">
+                                <div className="profileScreen__plan-details">
+                                    <span className="profileScreen__plan-heading">Netflix Premium</span>
+                                    <span className="profileScreen__plan-subHeading">1080p</span>
+                                </div>  
+                                <button className="profileScreen__plan-button">Current Package</button>
+                            </div>
+                        </div>
                         <button className="profileScreen__signout" onClick={()=> auth.signOut()}>Sign Out</button>
                     </div>
                 </div>
